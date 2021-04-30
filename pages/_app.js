abd504f7cf5c34ce75eab97e22c25f6dafc80e3b
julietaflux <julietaflux@gmail.com>
2021-04-30 17:23:43 -0300

    S33D feat: adding hoyjar

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,20 @@ function MyApp({ Component, pageProps }) {
           <Component {...pageProps} />
         </div>
       </div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(h,o,t,j,a,r){
+              h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+              h._hjSettings={hjid:2378519,hjsv:6};
+              a=o.getElementsByTagName('head')[0];
+              r=o.createElement('script');r.async=1;
+              r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+              a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+      `,
+        }}
+      ></script>
     </div>
   );
 }
